514686c9d91361063bc3da1babfad45c6ca78242
ryfazrin <riansera515@gmail.com>
2021-09-03 15:18:41 +0800

    add aturan variabel dalam js

---
+++ javascript-learning/2. Aturan Dasar, Variabel dan Konstanta/variabel_dalam_js.js
@@ -0,0 +1,12 @@
+var foo;
+
+foo = "Selamat Pagi";
+console.log(foo); // Selamat pagi
+
+foo = 1234.56;
+console.log(foo); // 1234.56
+
+foo = "Selamat Malam";
+console.log(foo) // Selamat Malam
+
+foo = false;
