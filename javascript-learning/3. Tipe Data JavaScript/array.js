730adfb3021315541e6c942c97a6a5ee990a63a0
ryfazrin <riansera515@gmail.com>
2021-09-04 09:15:36 +0800

    tipe data array

---
+++ javascript-learning/3. Tipe Data JavaScript/array.js
@@ -0,0 +1,18 @@
+var siswa = ["Andri", "Joko", "Sukma", "Rina", "Sari"];
+
+console.log(siswa[0]);
+console.log(siswa[1]);
+console.log(siswa[2]);
+console.log(siswa[3]);
+console.log(siswa[4]);
+
+console.log(siswa);
+
+// mengubah element array
+siswa[0] = "alex";
+console.log(siswa);
+
+// Array 2 dimensi
+var koordinat = [[2,5], [9,4],[3,1]];
+console.log(koordinat[0][0]);
+console.log(koordinat[1][1]);
