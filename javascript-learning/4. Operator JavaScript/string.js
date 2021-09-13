50912ba27c4b290d8700ddbf28978d899ffdae95
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-09-13 23:39:37 +0800

    update oerator string

--- javascript-learning/4. Operator JavaScript/string.js
+++ javascript-learning/4. Operator JavaScript/string.js
@@ -1,3 +1,27 @@
 var foo, bar, baz;
 
 foo = "Sedang " + "Belajar " + "JavaScript";
+console.log(foo);
+
+foo = "Belajar " + "ECMAScript " + 7;
+console.log(foo);
+
+foo = 'Selamat ';
+bar = "Malam ";
+baz = `Indonesia`;
+hasil = foo + bar + baz;
+console.log(hasil);
+
+var num;
+
+num = 10 + 10 + 9;
+console.log(num); // 29
+
+num = '10' + 10 + 9;
+console.log(num); // 10109
+
+num = 10 + '10' + 9;
+console.log(num); // 10109
+
+num = 10 + 10 + '9';
+console.log(num); // 209
