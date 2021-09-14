8d292035524fff658956e369b77a379a443800b2
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-09-14 23:37:24 +0800

    update operator assignment

--- javascript-learning/4. Operator JavaScript/assignment.js
+++ javascript-learning/4. Operator JavaScript/assignment.js
@@ -9,3 +9,17 @@ b = b + " dari buku JavaScript Uncover";
 
 console.log(a);
 console.log(b);
+
+/**
+ * Operator gabungan Assignment
+ */
+
+a = 12;
+a += 10;
+a += a + 5;
+console.log(a);
+
+b = "Belajar";
+b +=" JavaScript";
+b +=" dari buku JavaScript Uncover";
+console.log(b);
