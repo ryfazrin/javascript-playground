77cf4bb6b06228efaba5fd34efbdfc3bb490ef74
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-09-14 23:43:03 +0800

    add operator spread

---
+++ javascript-learning/4. Operator JavaScript/spread.js
@@ -0,0 +1,13 @@
+/**
+ * menggunakan tanda titik tiga kali (...) 
+ * kemudian diikuti dengan nama variabel.
+ */
+
+var nilai1 = ["a", "b", "c", "d"];
+console.log(nilai1); // Array [ "a", "b", "c", "d" ]
+
+var nilai2 = [1, 2, 3, 4];
+console.log(nilai2); // Array [ 1, 2, 3, 4 ]
+
+var nilai3 = [...nilai1, "e", "f"];
+console.log(nilai3);
